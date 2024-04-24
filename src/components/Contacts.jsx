c1f74cd371a19c0eb21cd4cba0635b025b5fbee0
MohammadRezaSaeeedi <M.R.Saeedi2024@gmail.com>
2024-04-24 08:57:12 +0330

    Mini-Project-React first commit

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import { v4 } from "uuid";
+import ContactsList from "./ContactsList";
+import inputs from "../constants/inputs.js";
+import styles from "./Contacts.module.css";
+
+function Contacts() {
+    const [contacts,setCantacts] = useState([]);
+    const [alert,setAlert] = useState("");
+    const [contact,setContact]= useState({
+        id:"",
+        name:"",
+        lastName:"",
+        email:"",
+        phone:"",
+    });
+    const changeHandler = (event) => {
+const name = event.target.name;
+const value = event.target.value;
+setContact((contact) => ({... contact,[name]:value}))
+    };
+    const addHandler = () => {
+        if(!contact.name || !contact.lastName || !contact.email || !contact.phone){
+            setAlert("Please enter valid data");
+            return
+        }
+        setAlert("");
+        const newContact = {... contact , id: v4()};
+        setCantacts(contacts => ([... contacts,newContact]));
+        setContact({
+            name:"",
+            lastName:"",
+            email:"",
+            phone:"",
+        })
+
+    }
+    const deleteHandler = id => {
+        const newContacts = contacts.filter((contact) => contact.id !== id);
+        setCantacts(newContacts);
+    }
+  return (
+    <div className={styles.container}>
+        <div className={styles.form}>
+            {
+                inputs.map((input,index )=> (<input  key={index} type={input.type} placeholder={input.placeholder} name={input.name} value={contact[input.name]} onChange={changeHandler} />))
+            }
+            {/* <input type="text" placeholder="Name" name="name" value={contact.name} onChange={changeHandler} />
+            <input type="text" placeholder="Last Name" name="lastName" value={contact.lastName} onChange={changeHandler} />
+            <input type="email" placeholder="Email" name="email" value={contact.email} onChange={changeHandler} />
+            <input type="number" placeholder="Phone" name="phone" value={contact.phone} onChange={changeHandler} /> */}
+            <button onClick={addHandler}>Add Contact</button>
+        </div>
+        <div className={styles.alert}>
+            {alert && <p>{alert}</p>}
+        </div>
+        <ContactsList contacts={contacts} deleteHandler={deleteHandler} />
+    </div>
+  )
+}
+
+export default Contacts
---
+++ src/components/ContactsItem.jsx
@@ -0,0 +1,14 @@
+import styles from "./ContactsItem.module.css"
+
+function ContactsItem({data:{id, name, lastName,email,phone},deleteHandler,}) {
+  return (
+    <li className={styles.item} >
+    <p>{name} {lastName}</p>
+    <p><span>📬</span>{email}</p>
+    <p><span>📞</span>{phone}</p>
+    <button onClick={() => deleteHandler(id)}>🗑</button>
+</li>
+  )
+}
+
+export default ContactsItem
